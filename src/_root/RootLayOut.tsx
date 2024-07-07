3cddd464e4d2079858414d8522a6098d48d11b47
Goddemunchies <52329758+Goddemunchies@users.noreply.github.com>
2024-07-07 05:37:08 +0300

    root conditional routing

--- src/_root/RootLayOut.tsx
+++ src/_root/RootLayOut.tsx
@@ -1,23 +1,29 @@
 import Bottombar from "@/components/shared/Bottombar"
 import Sidebar from "@/components/shared/Sidebar"
 import Topbar from "@/components/shared/Topbar"
-import { Outlet } from "react-router-dom"
+import Spinner from "@/components/ui/spinner"
+import useSelectorWithDelay from "@/lib/customHooks/useSelectorWithDelay"
+import { Navigate, Outlet } from "react-router-dom"
 
 const RootLayOut = () => {
+  const {isAuthed, timerExpired} = useSelectorWithDelay();
   return (
-    <div className="w-full flex flex-col">
-     <Topbar />
-
-      <div className="w-full md:flex h-full">
-          <Sidebar />
-          <section className="flex flex-1 h-full">
-            <Outlet />
-          </section> 
-        </div>
-
-        <Bottombar />
-
-  </div>
+    <>
+    {
+      isAuthed ? ( <div className="w-full flex flex-col">
+        <Topbar />
+   
+         <div className="w-full md:flex h-full">
+             <Sidebar />
+             <section className="flex flex-1 h-full">
+               <Outlet />
+             </section> 
+           </div>
+           <Bottombar />
+     </div>) : timerExpired ? (<Navigate to="/sign-in"/>) : (<Spinner />)
+    }
+    </>
+  
   )
 }
 
